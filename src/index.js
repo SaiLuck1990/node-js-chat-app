ca4b57fed7fc156b029449b6748eca61437c3e7b
Sailakshmi  Kumar <sailakshmikumaar@gmail.com>
2021-01-20 20:05:49 +0000

    Tracking users in room , storing users , rendering userList

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http = require('http')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = new express()
 const port = process.env.PORT || 3000
@@ -17,32 +18,58 @@ app.use(express.static(publicDirPath))
 io.on('connection', (socket) => {
     console.log('New websocket connection')
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({
+            id: socket.id,
+            ...options
+        })
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined the room !`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
 
         // socket.emit, io.emit, socket.broadcast.emit
         // io.to.emit, socket.broadcast.to.emit
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback('Profanity not allowed')
         }
-        io.emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback('Delivered')
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left !'))
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+
     })
 })
 
---
+++ src/utils/users.js
@@ -0,0 +1,62 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+
+    // clean the data 
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // validate the data 
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required'
+        }
+
+    }
+    // check for existing user 
+    const existingUser = users.find((user) => {
+        return user.room == room && user.username === username
+    })
+
+    // validate username 
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    // if -1 , found no match , 0 or greater for no match
+    if (index !== -1) {
+        // To remove items from array using index and second argument no of items to remove 
+        return users.splice(index, 1)[0]
+    }
+
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room
+    )
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
+
